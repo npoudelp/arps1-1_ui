3847cb7558cd040efa93ac59e09961841ee1b6c5
npoudelp <npoudelp@gmail.com>
2024-06-23 20:40:06 +0545

    form added to add field

--- pages/filed_management.php
+++ pages/filed_management.php
@@ -12,16 +12,49 @@ include_once("../partials/header.php");
 
 
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-6">
+        <div class="row mt-1">
+            <div class="col-md-8">
                 <div class="container-fluid" style="height: 70vh" id="map"></div>
             </div>
-            <div class="col-md-6">
-                <button class="btn btn-outline-dark" onclick="clearMap()">
-                    Reset
-                </button>
+            <div class="col-md-4">
+
                 <button class="btn btn-outline-dark" onclick="checkStatus()">checkStatus</button>
-                <p id="coordinates"></p>
+
+                <!-- filed add form -->
+
+                <div class="">
+                    <input type="hidden" name="" id="coordinates">
+                    <div class="form-group">
+                        <label for="field_name">Field Name</label>
+                        <input type="text" maxlength="35" class="form-control" id="field_name" placeholder="Enter field name">
+                    </div>
+                    <div class="form-group">
+                        <label for="nitrogen">Nitrogen Content</label>
+                        <input type="number" step="0.001" class="form-control" id="nitrogen" placeholder="Enter nitrogen content">
+                    </div>
+                    <div class="form-group">
+                        <label for="potassium">Potassium Content</label>
+                        <input type="number" step="0.001" class="form-control" id="potassium" placeholder="Enter potassium content">
+                    </div>
+                    <div class="form-group">
+                        <label for="weight">Weight</label>
+                        <input type="number" step="0.001" class="form-control" id="weight" placeholder="Enter weight">
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <button type="submit" onclick="addField()" class="btn btn-outline-warning">
+                                Add Field
+                            </button>
+                        </div>
+                        <div class="col-6">
+                            <button class="btn btn-outline-danger" onclick="clearMap()">
+                                Reset Map
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- filed add form ends -->
             </div>
         </div>
     </div>
@@ -30,6 +63,41 @@ include_once("../partials/header.php");
     include_once("../partials/map.php");
     ?>
     <script src="../js/tokenManager.js"></script>
+
+    <script>
+        addField = () => {
+            let field_name = $("#field_name").val();
+            let nitrogen = $("#nitrogen").val();
+            let potassium = $("#potassium").val();
+            let weight = $("#weight").val();
+            let coordinates = $("#coordinates").val();
+
+            if (field_name == "" || coordinates == "") {
+
+                return;
+            }
+
+            $.ajax({
+                url: "/api/field/add.php",
+                type: "POST",
+                data: {
+                    field_name: $("#field_name").val(),
+                    nitrogen: $("#nitrogen").val(),
+                    potassium: $("#potassium").val(),
+                    weight: $("#weight").val(),
+                    coordinates: $("#coordinates").val()
+                },
+                success: function(response) {
+                    console.log(response);
+                    alert("Field added successfully");
+                },
+                error: function(err) {
+                    console.log(err);
+                    alert("Error adding field");
+                }
+            })
+        }
+    </script>
 </body>
 
 </html>
