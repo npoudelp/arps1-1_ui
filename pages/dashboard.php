4fbb61719e7c8321d60bb1055727c5f6ac137da1
npoudelp <npoudelp@gmail.com>
2024-06-27 20:42:23 +0545

    landing page for user created

---
+++ pages/dashboard.php
@@ -0,0 +1,17 @@
+<?php
+include_once("../partials/header.php");
+
+?>
+
+<body>
+    <?php
+    include_once("../partials/navbar.php");
+    ?>
+
+
+
+    <?php
+    include_once("../partials/map.php");
+    ?>
+    <script src="../js/tokenManager.js"></script>
+</body>
