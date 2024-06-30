1bd0b574cda0af6ce161f56d69dfe12a8dc06f5e
npoudelp <npoudelp@gmail.com>
2024-06-30 16:00:24 +0545

    adding map, rendering added map integrated in same file

--- js/mapForDashboard.js
+++ js/mapForDashboard.js
@@ -38,8 +38,48 @@ async function initMap() {
     mapTypeId: google.maps.MapTypeId.HYBRID,
     zoom: 18,
   });
+  // from other page
+  // code to draw polygon
+  drawingManager = new google.maps.drawing.DrawingManager({
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [google.maps.drawing.OverlayType.POLYGON],
+    },
+    polygonOptions: {
+      strokeColor: "#FFFFFF",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      delete: true,
+    },
+  });
 
-  plotMap = (coordinates, id, name, nitrogen, phosphorus, potassium) => {
+  // get coordinates after drawing polygon
+  google.maps.event.addListener(drawingManager, "overlaycomplete", (event) => {
+    if (event.type === google.maps.drawing.OverlayType.POLYGON) {
+      const polygon = event.overlay;
+      const coordinates = polygon.getPath().getArray();
+      paths_to_draw.push(coordinates);
+      displayCoordinates();
+      geofenceArea.setPaths(paths_to_draw);
+    }
+  });
+
+  // create geofence area
+  const geofenceArea = new google.maps.Polygon({});
+
+  // check if current location is inside geofence area
+  isInsideGeofence = (point) => {
+    console.log(geofenceArea);
+    return google.maps.geometry.poly.containsLocation(point, geofenceArea);
+  };
+
+  drawingManager.setMap(map);
+
+  // other code ends here
+
+  plotMap = (coordinates, id, name, nitrogen, phosphorus, potassium, crop) => {
     paths_to_draw.push(coordinates);
 
     const values = coordinates.match(/{(.*?)}/g);
@@ -61,13 +101,29 @@ async function initMap() {
       fillOpacity: 0.35,
     });
 
-    coordinates_array.push(id, name, nitrogen, phosphorus, potassium);
+    coordinates_array.push(id, name, nitrogen, phosphorus, potassium, crop);
     available_fields.push(coordinates_array);
 
     plot.setMap(map);
 
+    //sets name for polygon
+    const infoWindow = new google.maps.InfoWindow({
+      content: name,
+    });
+
     plot.addListener("click", function (event) {
       // Handle click event
+      infoWindow.setPosition(event.latLng);
+      infoWindow.open(map, plot);
+      $("#field_display").show();
+      $("#coordinates").val(coordinates);
+      $("#name").val(name);
+      $("#nitrogen").val(nitrogen);
+      $("#phosphorus").val(phosphorus);
+      $("#potassium").val(potassium);
+      $("#crop").val(crop);
+      $("#update").val(id);
+
       console.log(name, event.latLng);
     });
 
@@ -89,26 +145,19 @@ async function initMap() {
   } else {
     console.log("Geolocation is not supported by this browser.");
   }
-
-  // create geofence area
-  const geofenceArea = new google.maps.Polygon({});
-
-  // check if current location is inside geofence area
-  isInsideGeofence = (point) => {
-    console.log(geofenceArea);
-    return google.maps.geometry.poly.containsLocation(point, geofenceArea);
-  };
 }
 
 clearMap = () => {
   paths_to_draw = [];
-  $("#coordinates").val(JSON.stringify(paths_to_draw));
+  $("#coordinates_value").val(JSON.stringify(paths_to_draw));
   initMap();
 };
 
 displayCoordinates = () => {
+  $("#field_display").hide();
+  $("#fieldAddForm").show();
   const flattenedPaths = paths_to_draw.flat();
-  $("#coordinates").val(JSON.stringify(flattenedPaths));
+  $("#coordinates_value").val(JSON.stringify(flattenedPaths));
 };
 
 checkStatus = () => {
@@ -159,7 +208,8 @@ getAllFields = () => {
           field.name,
           field.nitrogen,
           field.phosphorus,
-          field.potassium
+          field.potassium,
+          field.crop
         );
       });
     },
