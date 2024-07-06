c95897a8630fb1bdc2519ec4d224d9e22beee2e1
npoudelp <npoudelp@gmail.com>
2024-07-06 13:00:58 +0545

    what??

--- index.php
+++ index.php
@@ -6,9 +6,22 @@ include_once("./partials/header.php");
     <?php
     include_once("./partials/navbar.php");
     ?>
-<div class="p-5"></div>
-    
+    <div class="p-5">
+        <a href="./test.php" class="">scrape</a>
+    </div>
+
     <?php
     include_once("./partials/footer.php");
     ?>
+
+
+    <script>
+        $.ajax({
+            url: "https:www.dhm.gov.np/frontend_dhm/hydrology/getRainfallWatchMapBySeriesId",
+            type: "GET",
+            success: function(data) {
+                console.log(data);
+            }
+        });
+    </script>
 </body>
